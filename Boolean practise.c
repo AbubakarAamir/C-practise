5e4c9e03e19acdbe4c0b58cfe908aad7ae763af3
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-03-02 00:48:43 +0500

    Add files via upload

---
+++ Boolean practise.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdbool.h> //Jab bhi boolean data type use kare gae toh uski header use ho gi (stdbool.h):
+int main(){
+
+bool Abubakar_is_a_good_boy = true; //True means 1 (kiuke computer binary language samaj tha ha)
+bool Ali_is_not_a_good_boy = false; //False means 0 
+printf("%d\n",Abubakar_is_a_good_boy);
+printf("%d\n",Ali_is_not_a_good_boy);
+
+return 0;
+
+}
