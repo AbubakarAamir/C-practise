cb361b416a02a07986dfd4c744ba941e5f639588
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-03-03 01:59:07 +0500

    Add files via upload

---
+++ Information.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+
+    int age = 21;
+    char Name;
+    char Fathername;
+    char Religion;
+    char Address;
+    char Number;
+    char Country;
+    char Education;
+
+    printf("\n Enter your name\t");
+    scanf("%s",&Name);
+  printf("\n Enter your father-name\t");
+  scanf("%s",&Fathername);
+printf("\n Enter your religion\t");
+scanf("%s",&Religion);
+printf("\nEnter your address\t");
+scanf("%s",&Address);
+printf("\nEnter your number\t");
+scanf("%s",&Number);
+printf("\nEnter your country\t");
+scanf("%s",&Country);
+printf("\nEnter your age\t");
+scanf("%s",&age);
+
+printf("\nEnter your education\t");
+scanf("%s",&Education);
+
+
+return 0;
+
+
+
+
+}
