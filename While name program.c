fa3bac47f7f45a719d69cb123d4bf3a5d6fc500e
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-03-08 00:10:17 +0500

    Add files via upload

---
+++ While name program.c
@@ -0,0 +1,13 @@
+//Made by :Abubakar Aamir.
+//(This while condition is uesed to do repeatdly work)
+#include<stdio.h>
+int main(){
+	
+	int i = 1;
+	while(i<=10){
+		printf(" %d  Hello abubakar  :\n",i);
+		i++;
+	}
+	
+return 0;	
+}
