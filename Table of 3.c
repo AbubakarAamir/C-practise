ebce8c6b0405a7798642f7714f4d58cfed2f9b72
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-03-07 23:23:18 +0500

    Add files via upload

---
+++ Table of 3.c
@@ -0,0 +1,23 @@
+//Made by :Abubakat Aamir.
+
+// Table of 2 using while loop:
+
+
+
+//loops are used because they save time, reduce errors, and they make code more readable.
+  
+
+#include<stdio.h>
+int main (){
+	int num = 3;
+	int i = 1;
+	printf("Table of 3 using while loop\n");
+	// yeh while condition check kare ga (ke agar i ke value 10 se kaam ho gi toh yeh 10 thak print karde ga :warna agar i ke value 10 se jada ho gi toh condition false ho jai gi: 
+	 while(i<=10){
+	 	printf("%d x %d = %d \n",num,i,num*i);
+	 	i++;
+	 }
+	
+	
+	return 0;
+}
