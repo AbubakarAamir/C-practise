abbc6efdb7e0eed44306cd3067d45c0e59123676
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-03-15 01:48:30 +0500

    Add files via upload

---
+++ Arrays practise.c
@@ -0,0 +1,45 @@
+//Arrays practise:
+//Defination of Array: 
+//Array is waja se used karte ha kiuke iske andar multiple values stroes kar sakhte ha single variable ki.instead of declaring separate variable for each.
+
+#include<stdio.h>
+int main(){
+
+// int mynumbers = {1000,5000,3000,7000};
+
+//We have now created a variable that's name is (mynumbers) which holds an array of four integers.
+
+// int mynumbers []= {20,40,70,90};
+// printf("practise of Arrays\n");
+// printf("%d",& mynumbers[0]);
+
+//One more example:
+
+// printf("%d\n",mynumbers[3]);
+
+//Agar ksi array ko call karna hota ha toh uske index-number se call karte ha aur index number 0 se start ho tha ha :
+//Examples:
+//{1000,3000,6000,78900}
+//1000 = 0,3000 = 1, 6000 = 2, 78900 = 3
+
+
+// int mynumbers [] ={10000,20000,50000,19000,};
+// mynumbers[0] = 120000; // Aagar ksi  specific element ki value change karni ho gi toh, refer to the index number se kare gae :
+
+// printf("%d",mynumbers[0]);
+
+
+// Declare an array of four integers:(aik array banaya jismei four integars ha )
+
+// int myNumbers[4];
+
+// Add elements:
+// myNumbers[0] = 25;
+// myNumbers[1] = 50;
+// myNumbers[2] = 75;
+// myNumbers[3] = 100;
+
+
+    return 0;
+
+}
