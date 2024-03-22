168cccd4ced1bed082563b926ca7eaf6a97f15be
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-03-22 15:21:29 -0700

    Add files via upload

---
+++ String practise.c
@@ -0,0 +1,39 @@
+//Practise of String .
+//Made by :Abubakar Aamir.
+
+// Defination:
+//Strings are used for storing text/characters.
+//For example, "Hello World" is a string of characters.
+
+
+#include<stdio.h>
+int main(){
+
+// char hello [] = "Hello abubakar";
+// printf("%s",hello);
+
+
+// char hello[] = "kaise ho ap!";
+// printf("%c", hello[3]); 
+
+
+// char carName[] = "Vigo";
+// int i;
+
+// for (i = 0; i < 5; ++i) {
+//   printf("%c\n", carName[i]);
+// }
+
+
+// char message[] = "Good to see you,";
+// char fname[] = "ahmed";
+
+// printf("%s %s!", message, fname);
+
+
+
+
+
+
+    return 0;
+}
